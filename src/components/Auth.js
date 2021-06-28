66e77253c3403cf652e79cc4e2b6d6141e4d515d
alex <alexizana10@gmail.com>
2021-06-28 16:30:43 +0400

    added global state with useContext

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import Card from './UI/Card';
-import './Auth.css';
+import Card from "./UI/Card";
+import { AuthContext } from "../context/auth-context";
 
-const Auth = props => {
-  const loginHandler = () => {};
+import "./Auth.css";
+
+const Auth = (props) => {
+  const authContext = useContext(AuthContext);
+
+  const loginHandler = () => {
+    authContext.login();
+  };
 
   return (
     <div className="auth">
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
+import AuthContextProvider from "./context/auth-context";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>,
+  document.getElementById("root")
+);
